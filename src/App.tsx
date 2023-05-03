e0c8c2a4d9761e38109218418d32a05cb6742b84
Leandro Rangel <rangel94@outlook.com>
2023-05-03 11:21:16 -0300

    Páginas e rotas - React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from "./styles/themes/default";
-
-import { Button } from './components/Button';
+import { Router } from './Router';
 import { GlobalStyle } from './styles/global';
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
