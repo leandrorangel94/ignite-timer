d19b369b7ecb2e0179e4be774563fe2aa8d635ad
Leandro Rangel <rangel94@outlook.com>
2023-05-08 16:48:22 -0300

    Formulários - Validando formulários

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,14 +9,27 @@ import {
   TaskInput,
 } from "./styles";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O ciclo precisa ter no mínimo 5 minutos")
+    .max(60, "O ciclo precisa ter no máximo 60 minutos"),
+});
 export function Home() {
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  });
+
   function handleCreateNewCycle(data: any) {
     console.log(data);
   }
+
   const task = watch("task");
-  const isSubmitDisabled = !task.trim();
+  const isSubmitDisabled = !task;
 
   return (
     <HomeContainer>
